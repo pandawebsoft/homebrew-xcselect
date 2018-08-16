81e36e7cb9adc64ae93b5c8059c608aa2dc54671
PandaWebsoft <developer@pandawebsoft.com>
2018-08-16 21:34:57 +0900

    Update xcselect.rb

--- xcselect.rb
+++ xcselect.rb
@@ -7,7 +7,7 @@ class Xcselect < Formula
   homepage "https://github.com/pandawebsoft/xcselect"
   #url "https://github.com/pandawebsoft/xcselect/releases/download/#{HOMEBREW_HTTR_VERSION}/xcselect-release.tar.gz"
   url "https://github.com/pandawebsoft/xcselect/archive/v#{HOMEBREW_XCSELECT_VERSION}.tar.gz"
-  sha256 "6e5265b55d1f74ef1b83e9403cc5e8c90ca4dfd5ffaa95b799ee044fa19c042a"
+  sha256 "75639d12e5d91af4d4e0806518727d779bbf72b76197f44f7c08bf0cd71c4015"
   # depends_on "cmake" => :build
 
   version HOMEBREW_XCSELECT_VERSION
