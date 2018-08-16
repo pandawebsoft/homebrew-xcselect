d50479c9aabfced0cbea603ace47189308e68b81
PandaWebsoft <developer@pandawebsoft.com>
2018-08-17 00:15:17 +0900

    Update xcselect.rb

--- xcselect.rb
+++ xcselect.rb
@@ -1,17 +1,15 @@
-require "formula"
-
-HOMEBREW_XCSELECT_VERSION='1.1.0.14'
-
 class Xcselect < Formula
-  desc "A command line tool that manages multiple versions of Xcode."
-  homepage "https://github.com/pandawebsoft/xcselect"
-  url "https://github.com/pandawebsoft/xcselect/archive/v#{HOMEBREW_XCSELECT_VERSION}.tar.gz"
-  sha256 "75639d12e5d91af4d4e0806518727d779bbf72b76197f44f7c08bf0cd71c4015"
-
-  version HOMEBREW_XCSELECT_VERSION
-  head 'https://github.com/pandawebsoft/xcselect.git', :branch => 'master'
-
-  def install
-     bin.install "bin/xcselect"
-  end
+    desc "Manage multiple versions of Xcode"
+    homepage "https://github.com/pandawebsoft/xcselect"
+    url "https://github.com/pandawebsoft/xcselect/archive/v1.1.0.14.tar.gz"
+    sha256 "75639d12e5d91af4d4e0806518727d779bbf72b76197f44f7c08bf0cd71c4015"
+    
+    def install
+        bin.install "bin/xcselect"
+    end
+    
+    test do
+        assert_equal version.to_s, shell_output("#{bin}/xcselect --version|sed 's/[()]/./g'|rev|cut -c2-|rev").chomp
+    end
 end
+
